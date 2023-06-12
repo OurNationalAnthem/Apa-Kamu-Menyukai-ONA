363202ce617163b11df8f16acc6b80e50f965e89
OurNationalAnthem <136347251+OurNationalAnthem@users.noreply.github.com>
2023-06-12 22:22:39 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main>
       <h3>ONA Entertaiment | ONA娛樂</h3>
-      <img src="https://www.pngitem.com/pimgs/m/17-173589_transparent-yui-hirasawa-png-k-on-yui-stop.png">
       <Title>オナは唯平沢愛してるよ</Title>
       <h1>Apa Kamu Mencintai ONA</h1>
       <h2>Jika Iya Kalian Boleh Click Sebanyak Banyaknya</h2>
@@ -17,6 +16,7 @@ export default function Home() {
         </a>{" "}
        Untuk Cinta Yang Lebih ULTIMAKS
       </p>
-    </main>
+       <iframe width="853" height="480" src="https://www.youtube.com/embed/wT_5LwlRUco" title="Anthem Of China CCTV-1, 2023 Version ( 1-April-2023 )" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </main>
   );
 }
