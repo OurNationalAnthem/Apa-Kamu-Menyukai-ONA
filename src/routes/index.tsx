cc27c630b69126ea4091ada529efdf560a60d669
OurNationalAnthem <136347251+OurNationalAnthem@users.noreply.github.com>
2023-06-12 22:04:29 +0700

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,18 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
+      <div class="container">
+      <img class="mw-100" src="https://www.pngitem.com/pimgs/m/17-173589_transparent-yui-hirasawa-png-k-on-yui-stop.png" alt="Max-width 50%">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            
+            <span class="fs-4">ONA Entertaiment｜オナ娛樂 </span>
+          </a>
+    
+  
+        </header>
+      </div>
       <Title>オナは唯平沢愛してるよ</Title>
       <h1>Apa Kamu Mencintai ONA</h1>
       <h2>Jika Iya Kalian Boleh Click Sebanyak Banyaknya</h2>
