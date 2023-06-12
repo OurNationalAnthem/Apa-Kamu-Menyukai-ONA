cf5028bea21256d728dd2b10b10cc00f5f85c7de
OurNationalAnthem <136347251+OurNationalAnthem@users.noreply.github.com>
2023-06-12 21:23:59 +0700

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -8,11 +8,11 @@ export default function NotFound() {
       <HttpStatusCode code={404} />
       <h1>Page Not Found</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        Kunjungi Channel{" "}
+        <a href="https://www.youtube.com/channel/UCz5DSVzY-1CjwA5nlKmzVSA" target="_blank">
+          Our National Anthem
         </a>{" "}
-        to learn how to build SolidStart apps.
+        Untuk Informasi Lebih Lanjut (Walau Sebenarnya Nggak Ada Juga Informasi Di Situ)
       </p>
     </main>
   );
